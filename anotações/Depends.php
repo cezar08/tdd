2c6112f521cf23d276fd16cff298905ed165f3fa
cezar08 <cezar08@unochapeco.edu.br>
2016-03-14 00:27:57 -0300

    Update Depends.php

--- anotações/Depends.php
+++ anotações/Depends.php
@@ -2,34 +2,34 @@
 
 class PilhaTest extends PHPUnit_Framework_TestCase
 {
-	public function testVazio()
-	{
-		$pilha = array();
-		$this->assertEmpty($pilha);
+    public function testVazio()
+    {
+	$pilha = array();
+	$this->assertEmpty($pilha);
 		
-		return $pilha;
-	}
+	return $pilha;
+    }
 
-	/**
-	 * 
-	*@depends testVazio
-	*/
-	public function testPush(array $pilha)
-	{
-		array_push($pilha, 'foo');
-		$this->assertEquals('foo', $pilha[count($pilha)-1]);
-		$this->assertNotEmpty($pilha);
+    /**
+    * 
+    *@depends testVazio
+    */
+    public function testPush(array $pilha)
+    {
+	array_push($pilha, 'foo');
+	$this->assertEquals('foo', $pilha[count($pilha)-1]);
+	$this->assertNotEmpty($pilha);
 
-		return $pilha;
-	}
+	return $pilha;
+    }
 
-	/**
-	 * 
-	* @depends testPush
-	*/
-	public function testPop(array $pilha)
-	{
-		$this->assertEquals('foo', array_pop($pilha));
-		$this->assertEmpty($pilha);
-	}
+    /**
+    * 
+    * @depends testPush
+    */
+    public function testPop(array $pilha)
+    {
+	$this->assertEquals('foo', array_pop($pilha));
+	$this->assertEmpty($pilha);
+    }
 }
