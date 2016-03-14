4641e06a2c233ca646f6d2582da482cf56913dbe
cezar08 <cezar08@unochapeco.edu.br>
2016-03-14 00:30:19 -0300

    Update dataProvider.php

--- anotações/dataProvider.php
+++ anotações/dataProvider.php
@@ -1,24 +1,23 @@
 <?php
+
 class DadosTest extends PHPUnit_Framework_TestCase
 {
+   /**
+    * 
+    * @dataProvider provedor
+    */
+    public function testSoma($a, $b, $c)
+    {
+	$this->assertEquals($c, $a +$b);
+    }
 
-	/**
-	* @dataProvider provedor
-	*/
-
-	public function testSoma($a, $b, $c)
-	{
-		$this->assertEquals($c, $a +$b);
-	}
-
-	public function provedor(){
-		return array(
-			array(0, 0, 0),
-			array(0, 1, 1),
-			array(1, 0, 1),
-			array(1, 3, 4)
-		);
-
-	}
-
+    public function provedor()
+    {
+	return array(
+		array(0, 0, 0),
+		array(0, 1, 1),
+		array(1, 0, 1),
+		array(1, 3, 4)
+	);
+    }
 }
