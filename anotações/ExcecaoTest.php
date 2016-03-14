52c48976f9da1670f4a18f74937842e265970867
cezar08 <cezar08@unochapeco.edu.br>
2016-03-14 00:34:24 -0300

    Update ExcecaoTest.php

--- anotações/ExcecaoTest.php
+++ anotações/ExcecaoTest.php
@@ -4,22 +4,22 @@ class ExcecaoTest extends PHPUnit_Framework_TestCase
 {
     /**
     * 
-	* @expectedException InvalidArgumentException
-	* @expectedExceptionMessage Alguma Mensagem
-	*/
-	public function testExcecaoTemMensagemCerta()
-	{
-		throw new InvalidArgumentException('Alguma Mensagem', 20);
-	}
+    * @expectedException InvalidArgumentException
+    * @expectedExceptionMessage Alguma Mensagem
+    */
+    public function testExcecaoTemMensagemCerta()
+    {
+	throw new InvalidArgumentException('Alguma Mensagem', 20);
+    }
 	
-	/**
-	*	
-	* @expectedException InvalidArgumentException
-	* @expectedExceptionCode 20
-	*/
-	public function testExcecaoTemCodigoCerto()
-	{
-		throw new InvalidArgumentException('Alguma Mensagem', 20);
-	}
-	
+    /**
+    *	
+    * @expectedException InvalidArgumentException
+    * @expectedExceptionCode 20
+    */
+    public function testExcecaoTemCodigoCerto()
+    {
+	throw new InvalidArgumentException('Alguma Mensagem', 20);
+    }
+    
 }
